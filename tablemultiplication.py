6e0920c82e047e28b088af67d4cee013a170e907
nithin-sudo <sathramnithin@gmail.com>
2025-02-12 17:35:41 -0500

    staging delted files

--- tablemultiplication.py
+++
@@ -1,9 +0,0 @@
-# print the multiplication table of a number n
-n = int(input("Enter number:"))
-i = 1
-while i <= 10:
-    result = n * i
-    #the below statement has f string which simply means we can write our expression
-    print(f"{n} * {i} = {result}")
-    i += 1
-print("Loop ended")
